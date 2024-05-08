41036099f476e2c873b6613aa03b5c0896bc6b89
Francesco Tonin <francescotonint@gmail.com>
2024-05-08 14:15:09 +0200

    Character creation (frontend) and initizialize backend

---
+++ client/src/components/CharacterViewer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="character-viewer">
+    <h1>Character Viewer</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CharacterViewer',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
