a4cf9f6b4d2ba4e78a7c49a24f5e1dffc04bdf8c
Francesco Tonin <francescotonint@gmail.com>
2024-05-08 19:10:55 +0200

    Trying to debug mongodb

---
+++ server/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config;
+const express = require('express');
+const server = express();
+const cors = require('cors');
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
+const db= mongoose.connection;
+db.on('error', (error) => {console.log(error)});
+db.once('open', () => console.log("Connesso al database"));
+
+server.use(express.json());
+server.use(cors());
+
+server.get("/", (req, res) => {
+    res.send("Ciao");
+})
+
+server.listen(3000, () => console.log("Server online!"));
+
