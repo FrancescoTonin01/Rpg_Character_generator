a90b84a2d40381663d576aa67fb94123b99c2d3d
Francesco Tonin <francescotonint@gmail.com>
2024-05-10 21:02:15 +0200

    Css on something but still looking awful :<

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <button v-on:click="toggle='character-viewer'; getCharacters()">View all Character</button>
-    <button v-on:click="toggle='character-creator'">Create Character</button>
+    <div class="buttoncontainer">
+    <button class="button-3" role="button" v-on:click="toggle='character-viewer'; getCharacters()">View all Character</button>
+    <button class="button-3" v-on:click="toggle='character-creator'">Create Character</button>
+  </div>
     <CharacterViewer v-show="toggle==='character-viewer'" :characters="characters" @delete-item="deleteCharacter"/>
     <CharacterCreator v-show="toggle==='character-creator'"/>
 </div>
@@ -56,5 +58,66 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  height:100%;
+  width: 100%;
+
 }
+.buttoncontainer{
+  display: flex;
+  justify-content: space-between;
+  margin-right: 20%;
+  margin-left: 20%;
+}
+
+.button-3 {
+  appearance: none;
+  background-color: #2ea44f;
+  border: 1px solid rgba(27, 31, 35, .15);
+  border-radius: 6px;
+  box-shadow: rgba(27, 31, 35, .1) 0 1px 0;
+  box-sizing: border-box;
+  color: #fff;
+  cursor: pointer;
+  display: inline-block;
+  font-family: -apple-system,system-ui,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji";
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 20px;
+  padding: 6px 16px;
+  position: relative;
+  text-align: center;
+  text-decoration: none;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  vertical-align: middle;
+  white-space: nowrap;
+}
+
+.button-3:focus:not(:focus-visible):not(.focus-visible) {
+  box-shadow: none;
+  outline: none;
+}
+
+.button-3:hover {
+  background-color: #2c974b;
+}
+
+.button-3:focus {
+  box-shadow: rgba(46, 164, 79, .4) 0 0 0 3px;
+  outline: none;
+}
+
+.button-3:disabled {
+  background-color: #94d3a2;
+  border-color: rgba(27, 31, 35, .1);
+  color: rgba(255, 255, 255, .8);
+  cursor: default;
+}
+
+.button-3:active {
+  background-color: #298e46;
+  box-shadow: rgba(20, 70, 32, .2) 0 1px 0 inset;
+}
+
 </style>
--- client/src/components/CharacterCreator.vue
+++ client/src/components/CharacterCreator.vue
@@ -1,18 +1,22 @@
 <template>
+  <div class="bg">
     <div class="character-creator">
-      <h1>Character Creator</h1>
-      <label for="character-name">Character Name:</label>
-      <input type="text" id="character-name" v-model="name" placeholder="Enter a name">
-      <label for="professions-list">Character Profession</label>
-      <select id="professions-list" v-model="profession">
+        <div class="toptext">Character Creator</div>
+      <div class="charactername" for="character-name">Character Name:
+      <input type="text" id="character-name" v-model="name" placeholder="Enter a name"></div>
+      <div class="characterp" for="professions-list">Character Profession      </div>
+      <div class="custom-select" >
+        <select id="professions-list" v-model="profession">
         <option value="Mage">Mage</option>
         <option value="Thief">Thief</option>
         <option value="Warrior">Warrior</option>
       </select>
+    </div>
       <p>{{ name }}</p>
       <p>{{profession }}</p>
       <button v-on:click="postCharacter">Create Character</button>
     </div>
+  </div>
   </template>
   
   <script>
@@ -40,6 +44,35 @@
   
   <!-- Add "scoped" attribute to limit CSS to this component only -->
   <style scoped>
-  
+  .bg{
+    width: auto;
+    height: fit-content;
+
+  }
+  .character-creator{
+    background: rgb(63,94,251);
+    background: radial-gradient(circle, rgba(123, 255, 0, 0.527) 0%, rgb(44, 148, 36) 100%);
+    width: 35rem;
+    height:35rem;
+    margin:0 auto;
+    border-radius: 4rem;
+  }
+  .toptext{
+    margin-top: 2rem;  
+    font-size: 2rem;
+    font-weight: bold;
+    width: auto;
+    padding-top: 1rem;
+    height: fit-content;
+    margin-bottom: 2rem;
+  }
+  .charactername{
+    font-size: 120%;
+  }
+  .characterp{
+    font-size: 120%;
+  }
+
+
   </style>
   
